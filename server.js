e6c0fc95521505384c731c23ef5f561ccc80eacb
Volker Lanting <VolkerLanting@gmail.com>
2017-02-15 19:23:50 +0100

    Try to get this repo to work with github pages.

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.use('/libs', express.static('libs/'));
-app.use('/engine', express.static('engine/'));
-app.use('/assets', express.static('assets/'));
+app.use('/ablett/libs', express.static('libs/'));
+app.use('/ablett/engine', express.static('engine/'));
+app.use('/ablett/assets', express.static('assets/'));
 
-app.use('/tests', express.static('tests/'));
+app.use('/ablett/tests', express.static('tests/'));
 
-app.use('/finddaisy', express.static('findhorse'));
+app.use('/ablett/finddaisy', express.static('findhorse'));
 
 app.listen(3000);
